965a6ece70f92bb2914418436fe8ef2442649d29
Thignvs <thiagoneves44@gmail.com>
2023-06-09 13:07:48 -0300

    fix

--- src/components/FooterBar.jsx
+++ src/components/FooterBar.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import Logo from  '../assets/logo.svg'
 const FooterBar = () => {
     return (
-        <footer className='footer-bar'>
+        <div className='footer-bar'>
             <img className='logo' src={Logo} alt='logo'></img>
             <button className='black-btn'>Free Consultation</button>
-        </footer>
+        </div>
     )
 }
 
